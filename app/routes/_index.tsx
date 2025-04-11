1bedd27df48424ace1ef2d6252edbb7e8adb22a8
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-04-11 20:19:46 +0200

    feat: add initial Home component with routing and meta information

---
+++ app/routes/_index.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from 'react-router';
+import type { Route } from './+types/_index';
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: 'New React Router App' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div>
+      <p>This is the index route of your app.</p>
+      <Outlet />
+    </div>
+  );
+}

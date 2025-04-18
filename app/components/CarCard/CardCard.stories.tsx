cd75d406b237ec4a284b3ef4e0064845ff03c370
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-04-18 10:51:40 +0200

    add story for CarCard component with example data and layout

---
+++ app/components/CarCard/CardCard.stories.tsx
@@ -0,0 +1,45 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { CarCard } from './CarCard';
+import Image from '@/assets/cars/CA18g-FORD-GT40-n7-3rd-24h-Le-Mans-1969-SIDE-OK-colore-e-dimensione-768x512.webp';
+import type { Car } from '@/schemas/car';
+
+const meta: Meta<typeof CarCard> = {
+  title: 'Components/CarCard',
+  component: CarCard,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <div className="max-w-sm">
+        <Story />
+      </div>
+    ),
+  ],
+};
+
+const data: Car = {
+  id: '3',
+  brand: 'Ford',
+  model: 'GT40',
+  make: 'Slot.it',
+  name: '24h Le Mans / #7 - M. Hailwood, D. Hobbs',
+  scale: '1:32',
+  reference: 'CA18g',
+  year: 1966,
+  category: ['Le Mans'],
+  images: {
+    perspective_diagonal_front_left: Image,
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof CarCard>;
+
+// Base story with controls
+export const Default: Story = {
+  args: {
+    car: data,
+  },
+};

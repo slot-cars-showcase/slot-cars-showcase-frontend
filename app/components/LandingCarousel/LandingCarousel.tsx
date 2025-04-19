776f94348caf59fd529e38a515845e828f5528b2
Pau Velasco <pauvelascogarrofe@gmail.com>
2025-04-19 12:57:36 +0200

    refactor LandingCarousel component to accept images as props and remove hardcoded image imports

--- app/components/LandingCarousel/LandingCarousel.tsx
+++ app/components/LandingCarousel/LandingCarousel.tsx
@@ -1,23 +1,12 @@
 import Autoplay from 'embla-carousel-autoplay';
 import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel';
 import { useRef } from 'react';
-import FordGT from '@/assets/cars/CA18g-FORD-GT40-n7-3rd-24h-Le-Mans-1969-SIDE-OK-colore-e-dimensione-768x512.webp';
-import LanciaLC2 from '@/assets/cars/CA21g-Lancia-LC2-1000-Km-Monza-1998-SIDE.webp';
-import OpelCalibra from '@/assets/cars/CA36g-Calibra-n.16-2nd-Norisring-ITC-1996-SIDE.webp';
-import Porsche962C from '@/assets/cars/CA52c-Porsche-962C-n9-Jarama-1992-SIDE.webp';
-import Porsche962C2 from '@/assets/cars/CA02m-Porsche-956LH-n.17-24h-Le-Mans-1984-SIDE-995x663.webp';
-import BritishBarn from '@/assets/cars/CA19f-British-Barn-BB90R-Suzuka-1000-Km-1990-SIDE_1-995x663.webp';
 
-const images = [
-  FordGT,
-  LanciaLC2,
-  OpelCalibra,
-  Porsche962C,
-  Porsche962C2,
-  BritishBarn,
-];
+interface LandingCarouselProps {
+  images: string[];
+}
 
-export const LandingCarousel = () => {
+export const LandingCarousel = ({ images }: LandingCarouselProps) => {
   const plugin = useRef(
     Autoplay({
       delay: 3000,
